f95345ea4188099fe160d2d276cc7f865b7ec8f6
bdiaz28 <brittanymdiaz@yahoo.com>
2021-11-07 21:52:25 -0500

    fix err

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,15 +7,14 @@ router.get('/', (req, res) => {
     Post.findAll({
       attributes: [
         'id',
-        'post_url',
         'title',
-        'body',
+        'content',
         'created_at',
       ],
       include: [
         {
           model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          attributes: ['id', 'content', 'commenter_username', 'post_id', 'user_id', 'created_at'],
           include: {
             model: User,
             attributes: ['username']
