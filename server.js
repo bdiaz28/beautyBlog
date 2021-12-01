ba29333f50eb0cf7f165d0f982c955addedbcaf6
bdiaz28 <brittanymdiaz@yahoo.com>
2021-12-01 17:30:26 -0500

    fix

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const path = require('path');
-const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
 const helpers = require('./utils/helpers');
 
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: { expires: 15 * 60 * 1000},
-  resave: true,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
 const hbs = exphbs.create({ helpers });
+// add - helpers - between brackets
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const app = express();
+const PORT = process.env.PORT || 3006;
+
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { expires: 10 * 60 * 1000 },
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  }))
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
 
-app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
